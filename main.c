10ee1cc1466dc616ee6d5e39e200d986a8624d5b
EunjinBang <twin1963@sookmyung.ac.kr>
2022-09-20 17:15:59 +0900

    03

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]) { 
-	char c;
-	int i;
+	char text;
 	
-	printf("input a number :");
-	scanf("%c",&c);
+	printf("enter a character :");
+	scanf("%c", &text);
 	
-	i = c -'0';
-	printf("Tne input number is %i\n",i);
+	printf("the next character of %c (%d) is %c (%d)", text, text, text+1, text+1);
 	
 	return 0;
 }
